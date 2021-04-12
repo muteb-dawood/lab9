f9765e55f9fdd3cf1d353afc3fc03027b633c4a7
muteb-dawood <78134579+muteb-dawood@users.noreply.github.com>
2021-04-12 14:24:13 +0300

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from platform import python_version
+print (python_version())
+
+
+# In[7]:
+
+
+# filename.py
+'''This is sentces write by muteb. '''
+if __name__ == "__main__":
+    pass # 'pass' is a temporary placeholder.
+
+
+# In[8]:
+
+
+if __name__ == "__main__":
+    print("Hello, world!") # Indent with four spaces (NOT a tab).
+
+
+# In[9]:
+
+
+help(print)
+
+
+# In[10]:
+
+
+x,y = 1, 4
+z = x+y
+print(z%2)
+
+
+# In[11]:
+
+
+x==y
+
+
+# In[ ]:
+
+
+def mathematical_opritions (x,y):
+    if x==1:
+        x=0
+        add=
+
+
+# In[12]:
+
+
+$$v= \frac{4}{3}.\pi=3.141159.r^3
+
+
+# In[16]:
+
+
+def sphere_volume(r):
+    Pi=3.14159
+    v= 4/3 * Pi * r **3:
+    return v 
+
+if __name__=="__main__":
+    volume=sphere_volume(5)
+    print(volume)
+
+
+# In[17]:
+
+
+def information (course, prof, level = 8):
+    print(course + prof + "in " + str(level))
+information('IS653', 'prof','Muteb',10)    
+
+
+# In[ ]:
+
+
+
+
